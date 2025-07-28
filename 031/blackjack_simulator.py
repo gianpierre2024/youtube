0cac38b596f1fcd169801ea944f44f905844a253
gianpierre2024 <giandelliturri@gmail.com>
2025-07-28 20:04:19 +0500

    Update blackjack_simulator.py
    Add different strategies besides "never bust".

--- 031/blackjack_simulator.py
+++ 031/blackjack_simulator.py
@@ -3,143 +3,146 @@ import multiprocessing
 import math
 import time
 
-# configuration options
-simulations = 6000000
-num_decks = 4
-shuffle_perc = 75
+# Configuration
+headless = True
+simulations = 10000
+num_decks = 8
+shuffle_perc = 25.0 + random.random()*25  # Reshuffle when less than this % of cards remain
 
 def simulate(queue, batch_size):
-	deck = []
+    def new_deck():
+        std_deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4 * num_decks
+        random.shuffle(std_deck)
+        return std_deck[:]
 
-	def new_deck():
-		std_deck = [
-		  # 2  3  4  5  6  7  8  9  10  J   Q   K   A
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-			2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-		]
+    def draw_card(deck):
+        if not deck:
+            deck.extend(new_deck())
+        return deck.pop(0)
 
-		# add more decks
-		std_deck = std_deck * num_decks
+    def hand_value(cards):
+        total = sum(cards)
+        aces = cards.count(11)
+        while total > 21 and aces:
+            total -= 10
+            aces -= 1
+        return total
 
-		random.shuffle(std_deck)
+    def play_sub_hand(player, dealer, deck):
+        bet = 1
 
-		return std_deck[:]
+        # Double down logic (simple version: always double down on 2 cards)
+        if len(player) == 2:
+            player.append(draw_card(deck))
+            bet *= 2
+            return resolve_hand(player, dealer, deck, bet)
 
-	def play_hand():
-		dealer_cards = []
-		player_cards = []
+        # Hit until 21 or bust
+        while hand_value(player) < 21:
+            player.append(draw_card(deck))
 
-		# deal initial cards
-		player_cards.append(deck.pop(0))
-		dealer_cards.append(deck.pop(0))
-		player_cards.append(deck.pop(0))
-		dealer_cards.append(deck.pop(0))
+        return resolve_hand(player, dealer, deck, bet)
 
-		# deal player to 12 or higher
-		while sum(player_cards) < 12:
-			player_cards.append(deck.pop(0))
+    def resolve_hand(player, dealer, deck, bet):
+        # Dealer hits on soft 17
+        while True:
+            d_val = hand_value(dealer)
+            if d_val < 17:
+                dealer.append(draw_card(deck))
+            elif d_val == 17 and 11 in dealer:
+                dealer[dealer.index(11)] = 1  # Convert soft 17 to hard 17
+            else:
+                break
 
-		# deal dealer on soft 17
-		while sum(dealer_cards) < 18:
-			exit = False
-			# check for soft 17
-			if sum(dealer_cards) == 17:
-				exit = True
-				# check for an ace and convert to 1 if found
-				for i, card in enumerate(dealer_cards):
-					if card == 11:
-						exit = False
-						dealer_cards[i] = 1
+        p_val = hand_value(player)
+        d_val = hand_value(dealer)
 
-			if exit:
-				break
+        if p_val > 21:
+            return -1 * bet  # Player busts
+        if d_val > 21 or p_val > d_val:
+            return 1 * bet   # Player wins
+        if p_val == d_val:
+            return 0         # Push
+        return -1 * bet      # Player loses
 
-			dealer_cards.append(deck.pop(0))
+    def play_hand(deck):
+        dealer = [draw_card(deck), draw_card(deck)]
+        player = [draw_card(deck), draw_card(deck)]
 
-		p_sum = sum(player_cards)
-		d_sum = sum(dealer_cards)
+        # Check for natural Blackjack
+        player_val = hand_value(player)
+        dealer_val = hand_value(dealer)
 
-		# dealer bust
-		if d_sum > 21:
-			return 1;
-		# dealer tie
-		if d_sum == p_sum:
-			return 0;
-		# dealer win
-		if d_sum > p_sum:
-			return -1;
-		# dealer lose
-		if d_sum < p_sum:
-			return 1
+        if player_val == 21 and dealer_val != 21:
+            return 1.5  # Blackjack win
+        elif player_val == 21 and dealer_val == 21:
+            return 0    # Push
+        elif dealer_val == 21:
+            return -1   # Dealer blackjack
 
-	# starting deck
-	deck = new_deck()
+        # Check for possible split
+        if player[0] == player[1]:
+            split_hand1 = [player[0], draw_card(deck)]
+            split_hand2 = [player[1], draw_card(deck)]
+            return (
+                play_sub_hand(split_hand1, dealer[:], deck)
+                + play_sub_hand(split_hand2, dealer[:], deck)
+            )
+        else:
+            return play_sub_hand(player, dealer, deck)
 
-	# play hands
-	win = 0
-	draw = 0
-	lose = 0
-	for i in range(0, batch_size):
-		# reshuffle cards at shuffle_perc percentage
-		if (float(len(deck)) / (52 * num_decks)) * 100 < shuffle_perc:
-			deck = new_deck()
+    win = draw = lose = 0
+    deck = new_deck()
 
-		# play hand
-		result = play_hand()
+    for _ in range(batch_size):
+        if (float(len(deck)) / (52 * num_decks)) * 100 < shuffle_perc:
+            deck = new_deck()
 
-		# tally results
-		if result == 1:
-			win += 1
-		if result == 0:
-			draw += 1
-		if result == -1:
-			lose += 1
+        result = play_hand(deck)
 
-	# add everything to the final results
-	queue.put([win, draw, lose])
+        if result > 1:
+            win += 1
+        elif result == 1:
+            win += 1
+        elif result == 0:
+            draw += 1
+        else:
+            lose += 1
+
+    queue.put([win, draw, lose])
 
 
-start_time = time.time()
+if __name__ == '__main__':
+    start_time = time.time()
+    cpus = multiprocessing.cpu_count()
+    batch_size = int(math.ceil(simulations / float(cpus)))
 
-# simulate
-cpus = multiprocessing.cpu_count()
-batch_size = int(math.ceil(simulations / float(cpus)))
+    queue = multiprocessing.Queue()
+    processes = []
 
-queue = multiprocessing.Queue()
+    for i in range(cpus):
+        process = multiprocessing.Process(target=simulate, args=(queue, batch_size))
+        processes.append(process)
+        process.start()
 
-# create n processes
-processes = []
+    for proc in processes:
+        proc.join()
 
-for i in range(0, cpus):
-	process = multiprocessing.Process(target=simulate, args=(queue, batch_size))
-	processes.append(process)
-	process.start()
+    finish_time = time.time() - start_time
 
-# wait for everything to finish
-for proc in processes:
-	proc.join()
+    win = draw = lose = 0
+    for _ in range(cpus):
+        results = queue.get()
+        win += results[0]
+        draw += results[1]
+        lose += results[2]
 
-finish_time = time.time() - start_time
-
-# get totals
-win = 0
-draw = 0
-lose = 0
-
-for i in range(0, cpus):
-	results = queue.get()
-	win += results[0]
-	draw += results[1]
-	lose += results[2]
-
-print
-print ('  cores used: %d' % cpus)
-print ('  total simulations: %d' % simulations)
-print ('  simulations/s: %d' % (float(simulations) / finish_time))
-print ('  execution time: %.2fs' % finish_time)
-print ('  win percentage: %.2f%%'  % ((win / float(simulations)) * 100))
-print ('  draw percentage: %.2f%%' % ((draw / float(simulations)) * 100))
-print ('  lose percentage: %.2f%%' % ((lose / float(simulations)) * 100))
-print
+    print()
+    print(f'  cores used: {cpus}')
+    print(f'  total simulations: {simulations}')
+    print(f'  simulations/s: {int(float(simulations) / finish_time)}')
+    print(f'  execution time: {finish_time:.2f}s')
+    print(f'  win percentage: {(win / float(simulations)) * 100:.2f}%')
+    print(f'  draw percentage: {(draw / float(simulations)) * 100:.2f}%')
+    print(f'  lose percentage: {(lose / float(simulations)) * 100:.2f}%')
+    print()
